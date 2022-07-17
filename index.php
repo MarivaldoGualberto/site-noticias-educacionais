48a15cd0b4235433d27279a82441431320c78c7c
marivaldogualberto <marivaldo.rodrigues30@gmail.com>
2022-07-17 05:22:23 -0300

    Estilização pagina cadastro e login

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MGR NOTICIAS EDUCACIONAIS</title>
     <link rel="stylesheet" href="css.css">
-</head>
 
 <body>
     <!--CABEÇALHO-->
